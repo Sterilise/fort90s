d08c025165ac62da9ce255d8cdba7d0f60dde38e
Sterilise <m.e.a@live.co.uk>
2018-12-08 15:00:53 +0000

    updated frontend

--- frontend/game.js
+++ frontend/game.js
@@ -51,6 +51,7 @@ socket.on("typing", function(handler) {
     feedback.innerHTML = "<p><em>" + handler + "is typing a message... </em></p>"; 
 });
 
+
 //emit events
 //move rotate and fire
 function move(xDirection, yDirection) {
@@ -70,3 +71,46 @@ function fire() {
         
     })
 }
+
+
+
+
+
+
+
+
+
+//Create a Pixi Application
+let app = new PIXI.Application({ 
+    width: window.innerWidth, 
+    height: window.innerHeight,                       
+    antialias: true, 
+    transparent: false, 
+    resolution: 1
+  }
+);
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+
+//load an image and run the `setup` function when it's done
+PIXI.loader
+  .add("assets/textures/player.png")
+  .load(setup);
+
+//This `setup` function will run when the image has loaded
+function setup() {
+
+  //Create the cat sprite
+  let cat = new PIXI.Sprite(PIXI.loader.resources["assets/textures/player.png"].texture);
+  
+  //Add the cat to the stage
+  app.stage.addChild(cat);
+}
+
+
+
+
+
+
+

919ceea6a3592d2f5f280a7f61d231610d5e3cf9
alex4o <mrabonin@gmail.com>
2018-12-08 16:05:38 +0200

    fixed package json, and added events

--- backend/app.js
+++ backend/app.js
@@ -16,9 +16,49 @@ app.use(express.static("public"));
 //socket setup, socket io waits for clients to make a connection and sets one up between the two
 var io = socket(server);
 //on connection event (with a new client) you can call callback function and store socket object
+class Player {
+    constructor(id) {
+        this.id = id;
+        this.location = { x: 0, y: 0 }
+    }
+}
+
+class World {
+    constructor() {
+        this.players = []
+        this.block = []
+    }
+
+    update() {
+        
+    }
+}
+
+let players = {}
 
 io.on("connection", function(socket){
     console.log("made socket connection", socket.id);
+    players[socket.id] = new Player(socket.id);
+
+    socket.on("move", data => {
+        
+    });
+
+    socket.on("rotate", data => {
+
+    });
+
+    socket.on("fire", data => {
+
+    });
+
+    socket.on("break", data => {
+
+    });
+
+    socket.on("place", data => {
+
+    })
 
     socket.on("chat", function(data){
         io.sockets.emit("chat", data)
